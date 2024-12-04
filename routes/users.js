31f2860fcc707cc145631ce56f55eaeeefde9590
trantrunghieu <hieuttps41023@gmail.com>
2024-12-04 10:55:29 +0700

    a
    a

--- routes/users.js
+++ routes/users.js
@@ -110,18 +110,26 @@ router.post("/send-mail", async function(req, res, next) {
 });
 
 //Đăng nhập
+router.get("/all", async function(req, res) {
+    var list = await userModel.find();
+    res.json(list);
+});
+
 router.post("/login", async function(req, res) {
     try {
-        const { username, password } = req.body;
-        const checkUser = await usermodel.findOne({ username: username, password: password });
+        const { name, password } = req.body;
+        const checkUser = await userModel.findOne({ name: name, password: password });
+
         if (checkUser == null) {
-            res.status(200).json({ status: false, message: "Tài khoản hoặc mật khẩu không đúng", user });
+            res.status(401).json({ status: false, message: "Đăng nhập không thành công" });
         } else {
-            const token = JWT.sign({ username: username }, config.SECRETKEY, { expiresIn: '30s' });
-            const refreshtoken = JWT.sign({ username: username }, config.SECRETKEY, { expiresIn: '1d' });
-            res.status(200).json({ status: true, message: "Đăng nhập thành công", token: token, refreshtoken: refreshtoken });
+            const token = JWT.sign({ name: name }, config.SECRETKEY, { expiresIn: '30s' });
+            const refreshToken = JWT.sign({ name: name }, config.SECRETKEY, { expiresIn: '1d' });
+
+            res.status(200).json({ status: true, message: "Đăng nhập thành công", token: token, refreshToken: refreshToken });
         }
     } catch (e) {
-        res.status(400).json({ status: false, message: "Đã xảy ra lỗi" });
+        res.status(500).json({ status: false, message: "Có lỗi xảy ra trên server" });
     }
 });
+module.exports = router;
